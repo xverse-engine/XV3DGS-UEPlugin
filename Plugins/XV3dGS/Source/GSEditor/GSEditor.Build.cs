957918e7f16560cf2e834159b554c97d90007e15
xiangkailin <1252694478@qq.com>
2024-06-07 10:17:12 +0800

    add support to ue5.4

---
+++ Plugins/XV3dGS/Source/GSEditor/GSEditor.Build.cs
@@ -0,0 +1,62 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class GSEditor : ModuleRules
+{
+	public GSEditor(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		bUsePrecompiled = true;
+		PublicIncludePaths.AddRange(
+			new string[] {
+				// ... add public include paths required here ...
+			}
+			);
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				// ... add other private include paths required here ...
+			}
+			);
+			
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+				"InputCore",
+				"EditorFramework",
+				"EditorStyle",
+				"UnrealEd",
+				"LevelEditor",
+				"InteractiveToolsFramework",
+				"EditorInteractiveToolsFramework",
+                "AssetRegistry",
+                "GSRuntime",
+                "Niagara",
+                "LevelEditor"
+            }
+			);
+		
+		
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+			);
+	}
+}
